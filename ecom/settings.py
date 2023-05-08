5a33dd87acb7a839e76dab2564e77f88d31d5383
himanish-tecblic <himanishvyas.tecblic@gmail.com>
2023-05-08 12:31:58 +0530

    docker + redis done

--- ecom/settings.py
+++ ecom/settings.py
@@ -130,7 +130,7 @@ CACHES_TTL = 60 * 1500
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/0",
+        "LOCATION": "redis://redis:6379/0",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
